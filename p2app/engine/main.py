2e90cd93a21ff060b023ebcc726bec69021e5189
Alex Meng <almeng@uci.edu>
2023-02-14 17:59:40 -0800

    successfully opens, closes, and exits the database. Can do a basic search though it is not great

--- p2app/engine/main.py
+++ p2app/engine/main.py
@@ -9,7 +9,9 @@
 # which means that YOU WILL DEFINITELY NEED TO MAKE CHANGES TO THIS FILE.
 
 
-
+import sqlite3
+from p2app.events import*
+from p2app.events.continents import Continent
 class Engine:
     """An object that represents the application's engine, whose main role is to
     process events sent to it by the user interface, then generate events that are
@@ -19,7 +21,7 @@ class Engine:
 
     def __init__(self):
         """Initializes the engine"""
-        pass
+        self.connection = None
 
 
     def process_event(self, event):
@@ -29,4 +31,34 @@ class Engine:
         # This is a way to write a generator function that always yields zero values.
         # You'll want to remove this and replace it with your own code, once you start
         # writing your engine, but this at least allows the program to run.
-        yield from ()
+
+
+
+        if isinstance(event, OpenDatabaseEvent):
+
+            self.connection = sqlite3.connect(event.path())
+            cursor = self.connection.cursor()
+            yield DatabaseOpenedEvent(event.path())
+
+
+        if isinstance(event, StartContinentSearchEvent):
+            try:
+
+                cursor = self.connection.cursor()
+                cursor.execute('SELECT continent_id FROM continent WHERE continent_code =?',
+                               (event.continent_code(),))
+                row = cursor.fetchone()
+                if row is not None:
+                    yield ContinentSearchResultEvent(
+                        Continent(row, event.continent_code(), event.name()))
+            except sqlite3.Error:
+                yield DatabaseOpenFailedEvent('Failed to open database!')
+
+        if isinstance(event, CloseDatabaseEvent):
+            if self.connection is not None:
+                self.connection.close()
+                self.connection = None
+            yield DatabaseClosedEvent()
+
+        if isinstance(event, QuitInitiatedEvent):
+            yield EndApplicationEvent()
